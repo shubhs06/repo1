e1b5d47157e9a6b34b62323f0a228a99cff4a05c
Your Name <you@example.com>
2024-09-21 12:02:29 +0530

    dopne

---
+++ demo.java
@@ -0,0 +1,16 @@
+//Business logoc class 
+class Display{
+void show(){
+System.out.println("Hello world");
+}
+
+}
+//Execution logic class
+public class demo{
+static public void main(String abc[])
+{
+
+Display obj=new Display();
+obj.show();
+}
+}
